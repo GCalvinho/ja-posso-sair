3edcbc9ab5da80e4ce13d31bb260f701ea1a452c
Goncalo <oliveira.779@gmail.com>
2021-03-11 11:54:05 +0000

    First version - Ja posso sair 1.0

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+Vermelho
+#D1403F
+
+Cinzento
+#3F4448
+
+Verde
+#61B15A
--- src/main/java/pt/i9/template/Controllers/ViewsController.java
+++ src/main/java/pt/i9/template/Controllers/ViewsController.java
@@ -1,6 +1,7 @@
 package pt.i9.template.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,14 @@ import javax.annotation.Resource;
 
 @Controller
 public class ViewsController {
+    private static final Logger log = LoggerFactory.getLogger(ViewsController.class);
 
     //Views
     public static String HOMEPAGE_HTML = "index";
 
     //Messages
     public static String message = "Já posso sair?";
+    public static String increasing_indicator = "+";
 
     @Resource
     CovidStatusService covidStatusService;
@@ -24,14 +27,32 @@ public class ViewsController {
     @RequestMapping(value = "/")
     public String homepage(Model model){
 
-        model.addAttribute("mainMessage", message);
         CovidStatusResponse data = covidStatusService.fetchStatus();
 
+        model.addAttribute("mainMessage", message);
         model.addAttribute("country", data.getResponse().stream().findFirst().get().country);
         model.addAttribute("newCases", data.getResponse().stream().findFirst().get().getCases().getNewCases());
         model.addAttribute("day", data.getResponse().stream().findFirst().get().getDay());
+        model.addAttribute("finallyFree", verifyNewCasesIndicator(data));
 
         return HOMEPAGE_HTML;
+    }
+
+    public Boolean verifyNewCasesIndicator(CovidStatusResponse data){
+        Boolean finallyFree;
+
+        try{
+            if(data.getResponse().stream().findFirst().get().getCases().getNewCases().contains(increasing_indicator)){
+                finallyFree = Boolean.FALSE;
+            }else{
+                finallyFree = Boolean.TRUE;
+            }
+        }catch (Exception e) {
+            log.info("Not possible to determine Covid Data. Proceeding being careful!");
+            finallyFree = Boolean.FALSE;
+        }
+
+        return finallyFree;
     }
 
 }
