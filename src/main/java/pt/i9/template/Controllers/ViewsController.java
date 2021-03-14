c4a725453b4b19b33e353c33c947f4e17fadd671
Goncalo <oliveira.779@gmail.com>
2021-03-14 01:33:17 +0000

    UI finalized

--- src/main/java/pt/i9/template/Controllers/ViewsController.java
+++ src/main/java/pt/i9/template/Controllers/ViewsController.java
@@ -2,9 +2,11 @@ package pt.i9.template.Controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pt.i9.template.Data.CovidStatusEnum;
 import pt.i9.template.Data.CovidStatusResponse;
 import pt.i9.template.Integration.CovidStatusService;
 
@@ -20,6 +22,10 @@ public class ViewsController {
     //Messages
     public static String message = "Já posso sair?";
     public static String increasing_indicator = "+";
+    public static String decreasing_indicator = "-";
+
+    @Value( "${countryAtEmergencyStatus}" )
+    public Boolean countryAtEmergencyStatus;
 
     @Resource
     CovidStatusService covidStatusService;
@@ -33,26 +39,38 @@ public class ViewsController {
         model.addAttribute("country", data.getResponse().stream().findFirst().get().country);
         model.addAttribute("newCases", data.getResponse().stream().findFirst().get().getCases().getNewCases());
         model.addAttribute("day", data.getResponse().stream().findFirst().get().getDay());
-        model.addAttribute("finallyFree", verifyNewCasesIndicator(data));
+        model.addAttribute("covidStatus", verifyStatus());
+        model.addAttribute("numbersIndicator", verifyNewCasesIndicator(data));
 
         return HOMEPAGE_HTML;
     }
 
-    public Boolean verifyNewCasesIndicator(CovidStatusResponse data){
-        Boolean finallyFree;
+    public String verifyStatus(){
+        CovidStatusEnum actualStatus;
 
         try{
-            if(data.getResponse().stream().findFirst().get().getCases().getNewCases().contains(increasing_indicator)){
-                finallyFree = Boolean.FALSE;
-            }else{
-                finallyFree = Boolean.TRUE;
+            if(countryAtEmergencyStatus){
+                actualStatus = CovidStatusEnum.notAllowedToTakeAWalk;
+            }else if(!countryAtEmergencyStatus){
+                actualStatus = CovidStatusEnum.carefullyAllowedToTakeAWalk;
+            }
+            else{
+                actualStatus = CovidStatusEnum.allowedToTakeAWalk;
             }
         }catch (Exception e) {
             log.info("Not possible to determine Covid Data. Proceeding being careful!");
-            finallyFree = Boolean.FALSE;
+            actualStatus = CovidStatusEnum.carefullyAllowedToTakeAWalk;
         }
 
-        return finallyFree;
+        return actualStatus.toString();
+    }
+
+    public String verifyNewCasesIndicator(CovidStatusResponse data){
+        if(data.getResponse().stream().findFirst().get().getCases().getNewCases().contains(increasing_indicator)){
+            return increasing_indicator;
+        }else{
+            return decreasing_indicator;
+        }
     }
 
 }
---
+++ src/main/java/pt/i9/template/Data/CovidStatusEnum.java
@@ -0,0 +1,7 @@
+package pt.i9.template.Data;
+
+public enum  CovidStatusEnum {
+    allowedToTakeAWalk,
+    notAllowedToTakeAWalk,
+    carefullyAllowedToTakeAWalk
+}
